f375dc657d6080dafcbfc4fa425fd8efde7f5cd0
Henrik Hodne <dvyjones@dvyjones.com>
2012-05-15 01:39:59 +0200

    Add Travis testing

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject context-io "0.0.1-SNAPSHOT"
   :description "Context.IO API wrapper for Clojure"
   :url "https://github.com/dvyjones/context-io-clj"
+  :plugins [[lein-multi "1.1.0"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]
                  [clj-oauth "1.3.1-SNAPSHOT"]
